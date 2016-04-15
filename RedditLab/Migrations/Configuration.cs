3609b097e671eaa873a975869e0b3a198000e71b
Tee Link <tesuanseylink@gmail.com>
2016-04-14 21:42:20 -0500

    successful run with controller configured

--- RedditLab/Migrations/Configuration.cs
+++ RedditLab/Migrations/Configuration.cs
@@ -17,26 +17,20 @@ namespace RedditLab.Migrations
 
         protected override void Seed(RedditLabDbContext context)
         {
-      
-            var posts = Builder<Post>.CreateListOfSize(10).All()
+            
+            var posts = Builder<Post>.CreateListOfSize(20).All()
                 .With(p => p.UpVote = Faker.NumberFaker.Number(0, 100))
                 .With(p => p.DownVote = Faker.NumberFaker.Number(0, 100))
                 .With(p => p.Title = Faker.TextFaker.Sentences(1))
                 .With(p => p.Author = Faker.NameFaker.FirstName())
                 .With(p => p.Posted = Faker.DateTimeFaker.DateTime())
-        .Build();
-            //  This method will be called after migrating to the latest version.
+                .With(p => p.Link = Faker.InternetFaker.Url())
+                .With(p => p.Body = Faker.TextFaker.Sentences(12))
+.Build();
+                context.Posts.AddRange(posts);
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+
         }
+
     }
 }
